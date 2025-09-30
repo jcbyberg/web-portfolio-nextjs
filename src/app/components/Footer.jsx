a1419c6bcc6f7da6961c21873117184c0db953da
jcbyberg <jcbyberg@gmail.com>
2025-09-30 11:14:16 -0400

    Github and Linked in links

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/images/logo.png"
+            alt="JB Creative logo"
+            width={240}
+            height={96}
+            className="h-15 w-auto filter brightness-0 invert"
+            priority
+          />
+        </Link>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
@@ -12,3 +23,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -25,15 +25,16 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-80
+    ">
+      <div className="flex container lg:py-2 flex-wrap items-center justify-between mx-auto px-2 py-1">
         <Link href="/" className="flex items-center">
           <Image
             src="/images/logo.png"
             alt="JB Creative logo"
             width={240}
             height={96}
-            className="h-24 w-auto filter brightness-0 invert"
+            className="h-16 w-auto filter brightness-0 invert"
             priority
           />
         </Link>
